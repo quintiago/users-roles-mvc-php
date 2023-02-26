57714859231ffa08f47d337ced36881bf19e223b
quintiago <quintiagogarciadev@gmail.com>
2023-02-25 23:35:18 -0500

    buttons were removed and created in the side navigation menu and new pages were implemented, the layout of the users page was made and the redirection of the same was implemented.

---
+++ views/pages/users.php
@@ -0,0 +1,48 @@
+<div class="content-wrapper" style="min-height: 717px;">
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Administrar Usuarios</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card card-info card-outline">
+                        <div class="card-header">
+                            <button type="button" class="btn btn-success crear perfil" data-toggle="modal" data-toggle="#modal-create-profile">
+                                Crear nuevo usuario
+                            </button>
+                        </div>
+                        <br>
+                        <div class="card-body">
+                            <table class="table table-bordered table-striped dt-responsive tablaperfil" width="100%">
+                                <thead>
+                                    <tr>
+                                        <th style="width:10px">#</th>
+                                        <th>Nombre</th>
+                                        <th>Usuarios</th>
+                                        <th>Foto</th>
+                                        <th>Rol</th>
+                                        <th>Acciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="card-footer">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
--- views/template.php
+++ views/template.php
@@ -31,7 +31,6 @@
 </head>
 
 <body class="hold-transition skin-blue sidebar-mini">
-    <!-- Site wrapper -->
     <div class="wrapper">
 
         <?php include "modules/header.php"; ?>
@@ -42,51 +41,22 @@
 
         <!-- =============================================== -->
 
-        <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <section class="content-header">
-                <h1>
-                    Blank page
-                    <small>it all starts here</small>
-                </h1>
-                <ol class="breadcrumb">
-                    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                    <li><a href="#">Examples</a></li>
-                    <li class="active">Blank page</li>
-                </ol>
-            </section>
 
-            <!-- Main content -->
-            <section class="content">
+            <?php
+            if (isset($_GET["page"])) {
 
-                <!-- Default box -->
-                <div class="box">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">Title</h3>
+                if (
+                    $_GET["page"] == "users" ||
+                    $_GET["page"] == "roles"
+                ) {
 
-                        <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-                                <i class="fa fa-minus"></i></button>
-                            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-                                <i class="fa fa-times"></i></button>
-                        </div>
-                    </div>
-                    <div class="box-body">
-                        Start creating your amazing application!
-                    </div>
-                    <!-- /.box-body -->
-                    <div class="box-footer">
-                        Footer
-                    </div>
-                    <!-- /.box-footer-->
-                </div>
-                <!-- /.box -->
+                    include "pages/" . $_GET["page"] . ".php";
+                }
+            }
+            ?>
 
-            </section>
-            <!-- /.content -->
         </div>
-        <!-- /.content-wrapper -->
 
         <?php include "modules/footer.php"; ?>
 
